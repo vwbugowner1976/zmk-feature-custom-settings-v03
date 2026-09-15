6b301939ccae45e712265c58f109d8c7fe5bcfcb
vwbugowner1976 <vwbugowner1976@gmail.com>
2026-09-16 00:26:00 +0900

    compat: provide split relay include shim for lighting

---
+++ include/zmk/split/relay/event.h
@@ -0,0 +1,7 @@
+#pragma once
+
+/*
+ * Compatibility shim for the ZMK v0.3 DYA fork. Split relay event macros are
+ * exposed through zmk/event_manager.h in that tree.
+ */
+#include <zmk/event_manager.h>
